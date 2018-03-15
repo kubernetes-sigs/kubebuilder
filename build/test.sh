f62c18258a3ef2e040e992055b7e792a69458036
Phillip Wittrock <pwittroc@google.com>
2018-03-14 20:26:36 -0700

    Run e2e tests before building a release

--- build/test.sh
+++ build/test.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 
-set -x -e
+cp -r /workspace/_output/kubebuilder /tmp/kubebuilder/
 
-test/scripts/build_kubebuilder.sh
-
-#go build ./cmd/...
-#go build ./pkg/...
-#go test ./cmd/...
-#go test ./pkg/...
-
-# Create the working directory to test the repo setup
 export GOPATH=/tmp/go
 mkdir -p $GOPATH/src/github.com/kubernetes-sigs/kubebuilder-test/
 cd $GOPATH/src/github.com/kubernetes-sigs/kubebuilder-test/
 
-# Run the commands
-/tmp/kubebuilder/bin/kubebuilder init repo --domain sample.kubernetes.io
-/tmp/kubebuilder/bin/kubebuilder create resource --group insect --version v1beta1 --kind Bee
-/tmp/kubebuilder/bin/kubebuilder create resource --group insect --version v1beta1 --kind Wasp
-
+# Setup env vars
+export PATH=$PATH:/tmp/kubebuilder/bin/
 export TEST_ASSET_KUBECTL=/tmp/kubebuilder/bin/kubectl
 export TEST_ASSET_KUBE_APISERVER=/tmp/kubebuilder/bin/kube-apiserver
 export TEST_ASSET_ETCD=/tmp/kubebuilder/bin/etcd
+
+# Run the commands
+kubebuilder init repo --domain sample.kubernetes.io
+kubebuilder create resource --group insect --version v1beta1 --kind Bee
+kubebuilder create resource --group insect --version v1beta1 --kind Wasp
 
 # Verify the controller-manager builds and the tests pass
 go install github.com/kubernetes-sigs/kubebuilder-test/cmd/controller-manager
