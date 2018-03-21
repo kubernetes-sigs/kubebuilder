5dbb53f7fee14ca078c917093a0b3ea9fee32687
Phillip Wittrock <pwittroc@google.com>
2018-03-21 09:24:22 -0700

    Update generated code for business logic
    - kubebuilder generate

--- samples/full/controller/src/samplecontroller/pkg/inject/zz_generated.kubebuilder.go
+++ samples/full/controller/src/samplecontroller/pkg/inject/zz_generated.kubebuilder.go
@@ -1,18 +1,3 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package inject
 
 import (
@@ -22,6 +7,7 @@ import (
 	"samplecontroller/pkg/inject/args"
 
 	"github.com/kubernetes-sigs/kubebuilder/pkg/inject/run"
+	appsv1 "k8s.io/api/apps/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -39,6 +25,9 @@ func init() {
 		}
 
 		// Add Kubernetes informers
+		if err := arguments.ControllerManager.AddInformerProvider(&appsv1.Deployment{}, arguments.KubernetesInformers.Apps().V1().Deployments()); err != nil {
+			return err
+		}
 
 		if c, err := foo.ProvideController(arguments); err != nil {
 			return err
