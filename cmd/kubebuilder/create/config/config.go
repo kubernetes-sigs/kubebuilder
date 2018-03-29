8a85d7c02b5248cc03ef0141276b4cfb963ed5ed
Phillip Wittrock <pwittroc@google.com>
2018-03-29 08:46:08 -0700

    Support creating config only for CRDs and for adding namespaces to CRDs

--- cmd/kubebuilder/create/config/config.go
+++ cmd/kubebuilder/create/config/config.go
@@ -16,49 +16,63 @@ package config
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"log"
 	"path/filepath"
+
+	"github.com/spf13/cobra"
 )
 
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Generate installation config for the API",
-	Long: `Generate installation config for the API.  Includes:
-- Namespace
-- RBAC ClusterRole
-- RBAC ClusterRoleBinding
-- CRDs
-- Controller Deployment
+	Long: `Generate installation config for the API.
+
+May create config for just CRDs or for controller-manager and CRDs.
+`,
+	Example: `# Generate config to install controller-manager and CRDs into a cluster
+kubebuilder create config --controller-image myimage:v1 --name myextensionname
+
+# Generate config to install only CRDs
+kubebuilder create config --crds
+
+# Generate config to install controller-manager and CRDs using a Deployment
+kubebuilder create config --controller-image myimage:v1 --name myextensionname --controller-type deployment
+
+# Generate config file at a specific location
+kubebuilder create config --crds --output myextensionname.yaml
+
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		if controllerType != "statefulset" && controllerType != "deployment" {
 			fmt.Printf(
-				"Invalid value %s for --controller-type, must be statefulset or deployment\n", controllerType)
+				"Invalid value %s for --controller-type, must be statefulset or deployment.\n", controllerType)
 			return
 		}
-		if controllerImage == "" {
-			fmt.Printf("Must specify --controller-image\n")
+		if controllerImage == "" && !crds {
+			fmt.Printf("Must either specify --controller-image or set --crds.\n")
 			return
 		}
-		if name == "" {
-			fmt.Printf("Must specify --name\n")
+		if name == "" && !crds {
+			fmt.Printf("Must either specify the name of the extension with --name or set --crds.\n")
 			return
 		}
 		CodeGenerator{}.Execute()
-		log.Printf("Config written to hack/install.yaml")
+		log.Printf("Config written to %s", output)
 	},
 }
 
 var (
-	controllerType, controllerImage, name, output string
+	controllerType, controllerImage, name, output, crdNamespace string
+	crds                                                        bool
 )
 
 func AddCreateConfig(cmd *cobra.Command) {
 	cmd.AddCommand(configCmd)
 	configCmd.Flags().StringVar(&controllerType, "controller-type", "statefulset", "either statefulset or deployment.")
+	configCmd.Flags().BoolVar(&crds, "crds", false, "if set to true, only generate crd definitions")
+	configCmd.Flags().StringVar(&crdNamespace, "crd-namespace", "", "if set, install CRDs to this namespace.")
 	configCmd.Flags().StringVar(&controllerImage, "controller-image", "", "name of the controller container to run.")
-	configCmd.Flags().StringVar(&name, "name", "", "name of the installation.")
+	configCmd.Flags().StringVar(&name, "name", "", "name of the installation.  used to generate the namespace and resource names.")
 	configCmd.Flags().StringVar(&output, "output", filepath.Join("hack", "install.yaml"), "location to write yaml to")
 }
