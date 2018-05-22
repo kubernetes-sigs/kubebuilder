e4f95e9c48ae21f7bd3e7ddd83469fa924064f57
lichuqiang <lichuqiang@huawei.com>
2018-05-22 11:17:54 +0800

    Rollback base image to ubuntu

--- cmd/kubebuilder/initproject/dockerfile.go
+++ cmd/kubebuilder/initproject/dockerfile.go
@@ -89,7 +89,7 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o controller-manager ./cm
 RUN go test ./pkg/... ./cmd/...
 
 # Copy the controller-manager into a thin image
-FROM scratch
+FROM ubuntu:latest
 # RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 COPY --from=builder /go/src/{{ .Repo }}/controller-manager .
