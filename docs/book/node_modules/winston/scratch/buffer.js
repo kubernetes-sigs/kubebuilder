ff42debf8567a3812b8c55cdd6919e08391bc0bc
Mengqi Yu <mengqiy@google.com>
2018-09-07 12:14:23 -0700

    not check in arch and os specific node_modules

--- docs/book/node_modules/winston/scratch/buffer.js
+++
@@ -1,8 +0,0 @@
-var winston = require('../');
-
-const logger = winston.createLogger({
-  transports: [new winston.transports.Console]
-});
-
-var test = {a: "a", b: new Buffer(10)};
-logger.log('info', ' Test Log Message' , test);
