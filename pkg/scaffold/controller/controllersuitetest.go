0df2b10ec23e40c6a36587e5be70780d332ce7f6
Shane Utt <shaneutt@linux.com>
2019-01-21 06:49:48 -0500

    Remove race condition in templated controller test

--- pkg/scaffold/controller/controllersuitetest.go
+++ pkg/scaffold/controller/controllersuitetest.go
@@ -96,10 +96,10 @@ func SetupTestReconcile(inner reconcile.Reconciler) (reconcile.Reconciler, chan 
 func StartTestManager(mgr manager.Manager, g *gomega.GomegaWithT) (chan struct{}, *sync.WaitGroup) {
 	stop := make(chan struct{})
 	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
-		wg.Add(1)
+		defer wg.Done()
 		g.Expect(mgr.Start(stop)).NotTo(gomega.HaveOccurred())
-		wg.Done()
 	}()
 	return stop, wg
 }
