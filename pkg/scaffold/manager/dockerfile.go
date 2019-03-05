cfbe7bd13e9d364b57acca7301f73735b85fc040
kaitoy <kaitoy@pcap4j.org>
2019-03-05 22:25:26 +0900

    Improve Dockerfile template such that copying vendor dir is cached.

--- pkg/scaffold/manager/dockerfile.go
+++ pkg/scaffold/manager/dockerfile.go
@@ -41,9 +41,9 @@ FROM golang:1.10.3 as builder
 
 # Copy in the go src
 WORKDIR /go/src/{{ .Repo }}
-COPY pkg/    pkg/
 COPY cmd/    cmd/
 COPY vendor/ vendor/
+COPY pkg/    pkg/
 
 # Build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o manager {{ .Repo }}/cmd/manager
