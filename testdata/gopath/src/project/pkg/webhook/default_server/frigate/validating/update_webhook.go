8e3462546ce86d9ef818f5b40f3938c1e3495879
Solly Ross <sollyross@google.com>
2019-05-03 18:56:34 -0700

    Actually move test projects to testdata
    This performs the move of the actual files that's set up in the previous
    commit.

---
+++ testdata/gopath/src/project/pkg/webhook/default_server/frigate/validating/update_webhook.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2019 The Kubernetes authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validating
+
+import (
+	shipv1beta1 "project/pkg/apis/ship/v1beta1"
+
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/builder"
+)
+
+func init() {
+	builderName := "validating-update-frigate"
+	Builders[builderName] = builder.
+		NewWebhookBuilder().
+		Name(builderName + ".testproject.org").
+		Path("/" + builderName).
+		Validating().
+		Operations(admissionregistrationv1beta1.Update).
+		FailurePolicy(admissionregistrationv1beta1.Fail).
+		ForType(&shipv1beta1.Frigate{})
+}
