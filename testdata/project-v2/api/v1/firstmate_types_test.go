3b5dff5a7f251cc9be4c4f0721e442d3aa2fbcc3
Seth Pellegrino <spellegrino@newrelic.com>
2019-06-12 11:06:40 -0700

    fix: additonal->additional
    Fixes a minor typo in the templates

--- testdata/project-v2/api/v1/firstmate_types_test.go
+++ testdata/project-v2/api/v1/firstmate_types_test.go
@@ -42,7 +42,7 @@ var _ = Describe("FirstMate", func() {
 		// Add any teardown steps that needs to be executed after each test
 	})
 
-	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
+	// Add Tests for OpenAPI validation (or additional CRD features) specified in
 	// your API definition.
 	// Avoid adding tests for vanilla CRUD operations because they would
 	// test Kubernetes API server, which isn't the goal here.
