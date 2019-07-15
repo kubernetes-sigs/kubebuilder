583c1c070311caa2161f8e5fc615546c50940d7e
Mengqi Yu <mengqiy@google.com>
2019-07-15 14:54:05 -0700

    :book: fix broken pieces in our book

--- docs/book/src/cronjob-tutorial/testdata/project/main.go
+++ docs/book/src/cronjob-tutorial/testdata/project/main.go
@@ -22,6 +22,7 @@ import (
 
 	kbatchv1 "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -46,9 +47,10 @@ var (
 )
 
 func init() {
+	_ = clientgoscheme.AddToScheme(scheme)
 
-	kbatchv1.AddToScheme(scheme) // we've added this ourselves
-	batchv1.AddToScheme(scheme)
+	_ = kbatchv1.AddToScheme(scheme) // we've added this ourselves
+	_ = batchv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -81,13 +83,15 @@ func main() {
 	}
 	// +kubebuilder:docs-gen:collapse=old stuff
 
-	err = (&controllers.CronJobReconciler{
+	if err = (&controllers.CronJobReconciler{
 		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("CronJob"),
-		Scheme: mgr.GetScheme(), // we've added this ourselves
-	}).SetupWithManager(mgr)
-	if err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
+		Log:    ctrl.Log.WithName("controllers").WithName("Captain"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Captain")
+		os.Exit(1)
+	}
+	if err = (&batchv1.CronJob{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Captain")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
