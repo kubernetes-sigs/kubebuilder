4240869b44386a5e42c0343bdc2edc1250f1e195
Damien Lespiau <damien@weave.works>
2019-09-24 15:58:07 +0100

    scaffold: Don't hardcode the boilerplate path in the Makefile
    init has a --path argument that specifies where the boilerplate file should be
    written to. Unforunately, the Makefile generation wasn't taking it into
    account.

--- pkg/scaffold/v2/makefile.go
+++ pkg/scaffold/v2/makefile.go
@@ -94,7 +94,7 @@ vet:
 
 # Generate code
 generate: controller-gen
-	$(CONTROLLER_GEN) object:headerFile=./hack/boilerplate.go.txt paths="./..."
+	$(CONTROLLER_GEN) object:headerFile={{printf "%q" .BoilerplatePath}} paths="./..."
 
 # Build the docker image
 docker-build: test
