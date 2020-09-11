af30ba262e5ce1f4522fa71c0a2d12cc70f0ee14
Ish Shah <ishah@redhat.com>
2020-09-11 10:21:13 -0700

    fix crd path for multigroup

--- testdata/project-v3/controllers/suite_test.go
+++ testdata/project-v3/controllers/suite_test.go
@@ -54,7 +54,7 @@ var _ = BeforeSuite(func(done Done) {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "crd", "bases")},
 	}
 
 	var err error
