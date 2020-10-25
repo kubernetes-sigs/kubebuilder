3172134e259922164e48bad0cde30131879cdc4a
prafull01 <prafull.ladha@gmail.com>
2020-10-26 00:17:08 +0530

    :sparkles: Add all kubernetes auth plugins for Azure, OIDC and gcp

--- pkg/plugin/v3/scaffolds/internal/templates/main.go
+++ pkg/plugin/v3/scaffolds/internal/templates/main.go
@@ -201,10 +201,14 @@ package main
 import (
 	"flag"
 	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	%s
