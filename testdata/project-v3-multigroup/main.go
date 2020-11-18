55b8fd7a6ab7004796b0cb8fc7e0c31333ad8bf2
Chris Hein <me@chrishein.com>
2020-11-18 13:22:06 -0800

    regenerating the testdata
    Signed-off-by: Chris Hein <me@chrishein.com>

--- testdata/project-v3-multigroup/main.go
+++ testdata/project-v3-multigroup/main.go
@@ -73,7 +73,6 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-
 	opts := zap.Options{
 		Development: true,
 	}
