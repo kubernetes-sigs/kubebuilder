e9d0b566aad05e91c62b642eb53b7063d177ea63
Eric Stroczynski <ericstroczynski@gmail.com>
2020-12-01 21:21:44 -0800

    remove GOPROXY and add flag env variables to build script

--- build/build_kubebuilder.sh
+++ build/build_kubebuilder.sh
@@ -18,19 +18,57 @@
 # While it can be run locally, it is intended to be run by cloudbuild
 # in the goreleaser/goreleaser image.
 
-# echo_run echos then evaluates all args.
-function echo_run() {
-  echo $@
-  # Ensure process substitution is evaluated with eval.
-  eval $@
+function usage() {
+  echo "
+  This script runs goreleaser using the build/.goreleaser.yml config.
+  While it can be run locally, it is intended to be run by cloudbuild
+  in the goreleaser/goreleaser image.
+
+  GORELEASER_FLAGS: contains flags to pass to the goreleaser binary (default: only --config is set).
+
+  SNAPSHOT: if set to any value, runs goreleaser in snapshot mode with mock release notes (default: unset).
+
+  NOTES_FLAGS: contains flags to pass to the notes binary (sigs.k8s.io/kubebuilder-release-tools/notes).
+               Does nothing if SNAPSHOT is set. (default: unset).
+
+  Examples:
+
+  # Run in snapshot mode: fake release notes, nothing is published, binaries build in '$(pwd)/dist'
+  \$ SNAPSHOT=1 $0
+
+  # Add a release type to the release notes
+  \$ NOTES_FLAGS=\"-r beta\" $0
+"
 }
+
+# GORELEASER_FLAGS contains flags for goreleaser such that the binary can be run
+# in local/snapshot/prod mode from the same script.
+# NOTE: if --snapshot is in GORELEASER_FLAGS, the release is not published to GitHub
+# and the build is available under $PWD/dist.
+GORELEASER_FLAGS="${GORELEASER_FLAGS:-}"
+# NOTES_FLAGS contains flags for the release notes generator (see install_notes for details).
+NOTES_FLAGS="${NOTES_FLAGS:-}"
+# SNAPSHOT is set by the CLI flag parser if --snapshot is a passed flag.
+# If not set, release notes are not generated.
+SNAPSHOT="${SNAPSHOT:-}"
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -h|--help)
+    usage
+    exit 0
+    ;;
+  esac
+done
+
 
 # install_notes installs kubebuilder's release notes generator globally with name "notes".
 function install_notes() {
   local tmp=$(mktemp -d)
   pushd "$tmp"
   go mod init tmp
-  go get sigs.k8s.io/kubebuilder-release-tools/notes
+  # Get by commit because v0.1.1 cannot be retrieved via `go get`.
+  go get sigs.k8s.io/kubebuilder-release-tools/notes@4777888c377a26956f1831d5b9207eea1fa3bf29
   popd
   rm -rf "$tmp"
 }
@@ -38,40 +76,19 @@ function install_notes() {
 set -o errexit
 set -o pipefail
 
-# SNAPSHOT is set by the CLI flag parser if --snapshot is a passed flag.
-# If not set, release notes are not generated.
-SNAPSHOT=
-# GORELEASER_FLAGS sets up goreleaser flags such that it can be run
-# in local/snapshot/prod mode from the same script.
-# NOTE: if --snapshot is set, release is not published to GitHub
-# and the build is available under $PWD/dist.
-GORELEASER_FLAGS=
-
-while [[ $# -gt 0 ]]; do
-  key="$1"
-  case $key in
-    --snapshot)
-    # TODO(estroz): figure out how to generate snapshot release notes with the kubebuilder generator.
-    echo "Running in snapshot mode. Release notes will not be generated from commits."
-    notes="Mock Release Notes for $(git describe --tags --always --broken)"
-    GORELEASER_FLAGS="${key} --release-notes <(echo \"${notes}\")"
-    SNAPSHOT=1
-    shift
-    ;;
-    *)
-    GORELEASER_FLAGS="$GORELEASER_FLAGS ${key}"
-    shift
-    ;;
-  esac
-done
-
 # Generate real release notes.
 if [ -z "$SNAPSHOT" ]; then
   tmp_notes="$(mktemp)"
   trap "rm -f ${tmp_notes}" EXIT
   install_notes
-  notes | tee "$tmp_notes"
+  notes $NOTES_FLAGS | tee "$tmp_notes"
   GORELEASER_FLAGS="${GORELEASER_FLAGS} --release-notes=${tmp_notes}"
+else
+  # TODO(estroz): figure out how to generate snapshot release notes with the kubebuilder generator.
+  echo "Running in snapshot mode. Release notes will not be generated from commits."
+  notes="Mock Release Notes for $(git describe --tags --always --broken)"
+  GORELEASER_FLAGS="${GORELEASER_FLAGS} --snapshot --rm-dist --skip-validate --release-notes <(echo \"${notes}\")"
 fi
 
-echo_run goreleaser release --config=build/.goreleaser.yml --rm-dist --skip-validate $GORELEASER_FLAGS
+# eval to run process substitution.
+eval goreleaser release --config=build/.goreleaser.yml $GORELEASER_FLAGS
