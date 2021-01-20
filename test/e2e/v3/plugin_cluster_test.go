8d108aa84fb126e0e347a1e3b8078cd084b71c38
Eric Stroczynski <ericstroczynski@gmail.com>
2021-01-19 16:35:12 -0800

    test commit
    Signed-off-by: Eric Stroczynski <ericstroczynski@gmail.com>

--- test/e2e/v3/plugin_cluster_test.go
+++ test/e2e/v3/plugin_cluster_test.go
@@ -155,6 +155,11 @@ func Run(kbc *utils.TestContext) {
 		}
 		return nil
 	}
+	defer func() {
+		out, err := kbc.Kubectl.CommandInNamespace("describe", "all")
+		ExpectWithOffset(1, err).NotTo(HaveOccurred())
+		fmt.Fprintln(GinkgoWriter, out)
+	}()
 	EventuallyWithOffset(1, verifyControllerUp, time.Minute, time.Second).Should(Succeed())
 
 	By("granting permissions to access the metrics")
