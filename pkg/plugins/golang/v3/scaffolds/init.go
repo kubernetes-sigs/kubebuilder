d905233d141093ae705c1c468a29c12c1a3bd7a4
Eric Stroczynski <ericstroczynski@gmail.com>
2021-03-05 10:20:07 -0800

    (go/v3) bump controller-runtime to v0.7.2
    Signed-off-by: Eric Stroczynski <ericstroczynski@gmail.com>

--- pkg/plugins/golang/v3/scaffolds/init.go
+++ pkg/plugins/golang/v3/scaffolds/init.go
@@ -36,7 +36,7 @@ import (
 
 const (
 	// ControllerRuntimeVersion is the kubernetes-sigs/controller-runtime version to be used in the project
-	ControllerRuntimeVersion = "v0.7.0"
+	ControllerRuntimeVersion = "v0.7.2"
 	// ControllerToolsVersion is the kubernetes-sigs/controller-tools version to be used in the project
 	ControllerToolsVersion = "v0.4.1"
 	// KustomizeVersion is the kubernetes-sigs/kustomize version to be used in the project
--- testdata/project-v3-multigroup/controllers/sea-creatures/kraken_controller.go
+++ testdata/project-v3-multigroup/controllers/sea-creatures/kraken_controller.go
@@ -46,7 +46,7 @@ type KrakenReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.2/pkg/reconcile
 func (r *KrakenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = r.Log.WithValues("kraken", req.NamespacedName)
 
