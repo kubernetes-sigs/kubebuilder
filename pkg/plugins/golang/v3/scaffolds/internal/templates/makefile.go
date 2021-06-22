66a03115102bdeef872ebfc72a50e25c1d90a6d5
varshaprasad96 <varshaprasad96@gmail.com>
2021-06-21 21:17:29 -0700

    update testdata

--- pkg/plugins/golang/v3/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v3/scaffolds/internal/templates/makefile.go
@@ -110,9 +110,8 @@ vet: ## Run go vet against code.
 
 ENVTEST_ASSETS_DIR=$(shell pwd)/testbin
 test: manifests generate fmt vet ## Run tests.
-	mkdir -p ${ENVTEST_ASSETS_DIR}
-	test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/{{ .ControllerRuntimeVersion }}/hack/setup-envtest.sh
-	source ${ENVTEST_ASSETS_DIR}/setup-envtest.sh; fetch_envtest_tools $(ENVTEST_ASSETS_DIR); setup_envtest_env $(ENVTEST_ASSETS_DIR); go test ./... -coverprofile cover.out
+	go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+	go test ./... -coverprofile cover.out
 
 ##@ Build
 
