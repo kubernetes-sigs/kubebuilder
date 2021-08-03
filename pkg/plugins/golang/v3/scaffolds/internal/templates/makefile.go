4a905a7de6e3d989a20c97968da497d640c04f66
Varsha <varshaprasad96@gmail.com>
2021-08-03 10:43:03 -0700

    :bug: add the path to Kubebuilder assets in go/v3 (#2272)
    Currently, while setting up envtest and running controller
    tests in go/v3 projects, we look for the binary in the default
    location. With the changes made in `setup-envtest`, the kb assets
    are downloaded at tmp/k8s/<os_specific_location>. This fix sets
    the KUBEBUILDER_ASSETS var to point to the location where they
    are downloaded so that `make test` does not fail.
    
    Signed-off-by: varshaprasad96 <varshaprasad96@gmail.com>

--- pkg/plugins/golang/v3/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v3/scaffolds/internal/templates/makefile.go
@@ -62,6 +62,8 @@ const makefileTemplate = `
 IMG ?= {{ .Image }}
 # Produce CRDs that work back to Kubernetes 1.11 (no version conversion)
 CRD_OPTIONS ?= "crd:trivialVersions=true,preserveUnknownFields=false"
+# ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
+ENVTEST_K8S_VERSION = 1.21
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
@@ -109,7 +111,7 @@ vet: ## Run go vet against code.
 	go vet ./...
 
 test: manifests generate fmt vet envtest ## Run tests.
-	go test ./... -coverprofile cover.out
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) -p path)" go test ./... -coverprofile cover.out
 
 ##@ Build
 
@@ -152,7 +154,6 @@ kustomize: ## Download kustomize locally if necessary.
 ENVTEST = $(shell pwd)/bin/setup-envtest
 envtest: ## Download envtest-setup locally if necessary.
 	$(call go-get-tool,$(ENVTEST),sigs.k8s.io/controller-runtime/tools/setup-envtest@latest)
-	$(ENVTEST) use 1.21
 
 # go-get-tool will 'go get' any package $2 and install it to $1.
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
