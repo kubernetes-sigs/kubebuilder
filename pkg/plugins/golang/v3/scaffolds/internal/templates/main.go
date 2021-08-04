6d83d8aa632ca62ea227d7950288de3b2b3a7923
Roman Dodin <dodin.roman@gmail.com>
2021-08-04 17:36:42 +0200

    fix error log message

--- pkg/plugins/golang/v3/scaffolds/internal/templates/main.go
+++ pkg/plugins/golang/v3/scaffolds/internal/templates/main.go
@@ -260,7 +260,7 @@ func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
 {{- end }}
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+		setupLog.Error(err, "unable to create manager")
 		os.Exit(1)
 	}
 
