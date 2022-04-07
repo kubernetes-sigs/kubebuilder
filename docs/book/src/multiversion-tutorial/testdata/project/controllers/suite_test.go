dff39efe264d3602855842f52f916f5b95464ee7
王然 <ranwang@alauda.io>
2022-04-07 14:23:00 +0800

    fix: suite test using global cfg and update docs

--- docs/book/src/multiversion-tutorial/testdata/project/controllers/suite_test.go
+++ docs/book/src/multiversion-tutorial/testdata/project/controllers/suite_test.go
@@ -58,7 +58,9 @@ var _ = BeforeSuite(func() {
 		ErrorIfCRDPathMissing: true,
 	}
 
-	cfg, err := testEnv.Start()
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
