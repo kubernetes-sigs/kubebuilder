d1b6eec19e3bc0e438a52fb5da4d2521f21cbfb0
Nikhil Sharma <nikhilsharma230303@gmail.com>
2022-05-14 23:56:30 +0530

    Added condition in test.sh and test_e2e.sh so that prow tests are skipped when the changes are only in docs

--- test_e2e.sh
+++ test_e2e.sh
@@ -13,5 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-./test/e2e/ci.sh
+CHECK_DOCS_ONLY=$("test/check-docs-only.sh")
+if [ -z "$CHECK_DOCS_ONLY" ]; then
+    ./test/e2e/ci.sh
+    exit 0
+fi
+echo "WARNING: The tests were skipped because only changes on the docs were faced"
