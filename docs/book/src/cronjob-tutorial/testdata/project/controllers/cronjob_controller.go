bebf3d5b7d164005e44f4530f46fa842e129e9b4
Priyanshu Raj <prraj@redhat.com>
2022-07-12 16:05:53 +0530

    nit fix

--- docs/book/src/cronjob-tutorial/testdata/project/controllers/cronjob_controller.go
+++ docs/book/src/cronjob-tutorial/testdata/project/controllers/cronjob_controller.go
@@ -248,7 +248,7 @@ func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	log.V(1).Info("job count", "active jobs", len(activeJobs), "successful jobs", len(successfulJobs), "failed jobs", len(failedJobs))
 
 	/*
-		Using the date we've gathered, we'll update the status of our CRD.
+		Using the data we've gathered, we'll update the status of our CRD.
 		Just like before, we use our client.  To specifically update the status
 		subresource, we'll use the `Status` part of the client, with the `Update`
 		method.
