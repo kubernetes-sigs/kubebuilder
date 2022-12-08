cda6171734575608f03088ee417e96f1af2b83a9
Jason Jung <jason1028kr@gmail.com>
2022-12-08 00:52:22 -0800

    🌱 Change testdata suffix and regenerate samples (#3111)
    * edit script
    
    * make generate
    
    * fix test name
    
    * project-v3-with-grafana

--- test/testdata/test.sh
+++ test/testdata/test.sh
@@ -36,7 +36,7 @@ test_project project-v3-multigroup
 test_project project-v3-declarative-v1
 test_project project-v3-config
 test_project project-v3-with-deploy-image
-test_project project-v3-with-metrics
+test_project project-v3-with-grafana
 
 # Project version v4-alpha
 test_project project-v4
@@ -44,4 +44,4 @@ test_project project-v4-multigroup
 test_project project-v4-declarative-v1
 test_project project-v4-config
 test_project project-v4-with-deploy-image
-test_project project-v4-with-metrics
+test_project project-v4-with-grafana
