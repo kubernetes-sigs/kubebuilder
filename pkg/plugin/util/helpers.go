797d1725732f4693c7dd2f56c98f73dd10d99ca9
Varsha <varshaprasad96@gmail.com>
2023-03-07 04:19:38 -0500

    🐛 Clarify deprecate documentation for API methods which are served to work with CRD/WebHook API versions v1beta1 (#3261)

--- pkg/plugin/util/helpers.go
+++ pkg/plugin/util/helpers.go
@@ -20,19 +20,22 @@ import (
 	"sigs.k8s.io/kubebuilder/v3/pkg/config"
 )
 
-// Deprecated: go/v4 no longer supports v1beta1 option
 // HasDifferentCRDVersion returns true if any other CRD version is tracked in the project configuration.
+// Deprecated: v1beta1 CRDs and Webhooks are not supported since k8s 1.22.
+// The features that allow users to scaffold projects using these API versions are deprecated and are no longer supported.
 func HasDifferentCRDVersion(config config.Config, crdVersion string) bool {
 	return hasDifferentAPIVersion(config.ListCRDVersions(), crdVersion)
 }
 
-// Deprecated: go/v4 no longer supports v1beta1 option
 // HasDifferentWebhookVersion returns true if any other webhook version is tracked in the project configuration.
+// Deprecated: v1beta1 CRDs and Webhooks are not supported since k8s 1.22.
+// The features that allow users to scaffold projects using these API versions are deprecated and are no longer supported.
 func HasDifferentWebhookVersion(config config.Config, webhookVersion string) bool {
 	return hasDifferentAPIVersion(config.ListWebhookVersions(), webhookVersion)
 }
 
-// Deprecated: go/v4 no longer supports v1beta1 option
+// Deprecated: v1beta1 CRDs and Webhooks are not supported since k8s 1.22.
+// The features that allow users to scaffold projects using these API versions are deprecated and are no longer supported.
 func hasDifferentAPIVersion(versions []string, version string) bool {
 	return !(len(versions) == 0 || (len(versions) == 1 && versions[0] == version))
 }
