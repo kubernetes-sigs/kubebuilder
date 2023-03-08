349d3f47fa0857a28b776065f3c7b079496572fc
Varsha Prasad Narsing <varshaprasad96@gmail.com>
2023-03-08 11:43:30 -0500

    [fix: go/v3, go/v4] Remove stale imports
    This PR removes the stale import: "pkg/envtest/printer" that is not needed
    after migrating to ginko/v2.
    
    Controller-runtime v0.14.0+ has migrated to use ginko/v2 and thereby removed
    "printer" package. The imports were still being scaffolded though not used in the
    codebase.
    
    Signed-off-by: Varsha Prasad Narsing <varshaprasad96@gmail.com>
    (cherry picked from commit 82843113fbf91d1b994de033dfa90a1f0533f49b)

--- pkg/plugins/golang/v3/scaffolds/internal/templates/controllers/controller_suitetest.go
+++ pkg/plugins/golang/v3/scaffolds/internal/templates/controllers/controller_suitetest.go
@@ -138,7 +138,6 @@ import (
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	%s
