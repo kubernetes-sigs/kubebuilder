6ee6e8bb77c594ffbf3a9887374b15531ae4c4fe
Camila Macedo <camilamacedo86@gmail.com>
2023-03-11 10:25:25 +0000

    :seedleing: stop to generate all samples for go/v3

--- test/testdata/generate.sh
+++ test/testdata/generate.sh
@@ -129,15 +129,10 @@ build_kb
 # [Deprecated] - Project version 2 uses plugin go/v2 (default).
 scaffold_test_project project-v2 --project-version=2
 
-# [Currently, default CLI plugin] - Project version 3 (default) uses plugin go/v3 (default).
+# [Deprecated] - Project version 3 (default) uses plugin go/v3 (default).
 scaffold_test_project project-v3
-scaffold_test_project project-v3-multigroup
-scaffold_test_project project-v3-declarative-v1 --plugins="go/v3,declarative"
-scaffold_test_project project-v3-config --component-config
-scaffold_test_project project-v3-with-deploy-image
-scaffold_test_project project-v3-with-grafana
 
-# [Next version, alpha] Project version v4-alpha
+# [Currently, default CLI plugin] - [Next version, alpha] Project version v4-alpha
 scaffold_test_project project-v4 --plugins="go/v4"
 scaffold_test_project project-v4-multigroup --plugins="go/v4"
 scaffold_test_project project-v4-declarative-v1 --plugins="go/v4,declarative"
