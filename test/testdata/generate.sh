88c94d5e855e7daef9c9e2bf1e9621f881d8678d
Camila Macedo <camilamacedo86@gmail.com>
2023-03-11 12:06:07 +0000

    :bug: fix sample project-v4-grafana

--- test/testdata/generate.sh
+++ test/testdata/generate.sh
@@ -130,12 +130,12 @@ build_kb
 scaffold_test_project project-v2 --project-version=2
 
 # [Currently, default CLI plugin] - Project version 3 (default) uses plugin go/v3 (default).
-scaffold_test_project project-v3
-scaffold_test_project project-v3-multigroup
+scaffold_test_project project-v3 --plugins="go/v3"
+scaffold_test_project project-v3-multigroup --plugins="go/v3"
 scaffold_test_project project-v3-declarative-v1 --plugins="go/v3,declarative"
-scaffold_test_project project-v3-config --component-config
-scaffold_test_project project-v3-with-deploy-image
-scaffold_test_project project-v3-with-grafana
+scaffold_test_project project-v3-config --component-config --plugins="go/v3"
+scaffold_test_project project-v3-with-deploy-image --plugins="go/v3"
+scaffold_test_project project-v3-with-grafana --plugins="go/v3"
 
 # [Next version, alpha] Project version v4-alpha
 scaffold_test_project project-v4 --plugins="go/v4"
@@ -143,4 +143,4 @@ scaffold_test_project project-v4-multigroup --plugins="go/v4"
 scaffold_test_project project-v4-declarative-v1 --plugins="go/v4,declarative"
 scaffold_test_project project-v4-config --component-config --plugins="go/v4"
 scaffold_test_project project-v4-with-deploy-image --plugins="go/v4"
-scaffold_test_project project-v4-with-grafana
+scaffold_test_project project-v4-with-grafana --plugins="go/v4"
