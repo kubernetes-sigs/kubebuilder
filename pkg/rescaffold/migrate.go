38b2e3954241949b8db29249d598f4d05c74bda0
yyy1000 <992364620@qq.com>
2023-07-07 17:02:25 +0800

    feat: add support for create API
    fix: golang lint
    
    fix: uppercase for Kind
    
    fix:nit
    
    fix: better name
    
    fix: add resource flag
    
    feat: only namespaced when flag set

--- pkg/rescaffold/migrate.go
+++ pkg/rescaffold/migrate.go
@@ -23,6 +23,7 @@ import (
 	"sigs.k8s.io/kubebuilder/v3/pkg/config/store"
 	"sigs.k8s.io/kubebuilder/v3/pkg/config/store/yaml"
 	"sigs.k8s.io/kubebuilder/v3/pkg/machinery"
+	"sigs.k8s.io/kubebuilder/v3/pkg/model/resource"
 	"sigs.k8s.io/kubebuilder/v3/pkg/plugin/util"
 )
 
@@ -54,6 +55,10 @@ func (opts *MigrateOptions) Rescaffold() error {
 	// call edit subcommands to enable or disable multigroup layout
 	if err := kubebuilderEdit(config); err != nil {
 		log.Fatalf("Failed to run edit subcommand %v", err)
+	}
+	// create APIs
+	if err := kubebuilderCreate(config); err != nil {
+		log.Fatalf("Failed to run create API subcommand %v", err)
 	}
 	return nil
 }
@@ -119,6 +124,21 @@ func kubebuilderEdit(store store.Store) error {
 	return nil
 }
 
+func kubebuilderCreate(store store.Store) error {
+	resources, err := store.Config().GetResources()
+	if err != nil {
+		return err
+	}
+
+	for _, r := range resources {
+		if err = createAPI(r); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func getInitArgs(store store.Store) []string {
 	var args []string
 	plugins := store.Config().GetPluginChain()
@@ -129,6 +149,50 @@ func getInitArgs(store store.Store) []string {
 	domain := store.Config().GetDomain()
 	if domain != "" {
 		args = append(args, "--domain", domain)
+	}
+	return args
+}
+
+func createAPI(resource resource.Resource) error {
+	var args []string
+	args = append(args, "create")
+	args = append(args, "api")
+	args = append(args, getAPIGVKFlags(resource)...)
+	args = append(args, getAPIResourceFlags(resource)...)
+	return util.RunCmd("kubebuilder create api", "kubebuilder", args...)
+}
+
+func getAPIGVKFlags(resource resource.Resource) []string {
+	var args []string
+
+	if len(resource.Group) > 0 {
+		args = append(args, "--group", resource.Group)
+	}
+	if len(resource.Version) > 0 {
+		args = append(args, "--version", resource.Version)
+	}
+	if len(resource.Kind) > 0 {
+		args = append(args, "--kind", resource.Kind)
+	}
+	return args
+}
+
+func getAPIResourceFlags(resource resource.Resource) []string {
+	var args []string
+	if resource.API == nil || resource.API.IsEmpty() {
+		// create API without creating resource
+		args = append(args, "--resource=false")
+	} else {
+		args = append(args, "--resource")
+		if resource.API.Namespaced {
+			args = append(args, "--namespaced")
+		}
+	}
+
+	if resource.Controller {
+		args = append(args, "--controller")
+	} else {
+		args = append(args, "--controller=false")
 	}
 	return args
 }
