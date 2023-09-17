e769e769d3601df7aad127592a9dda769645e3c1
dragon-fly <lhui_wang@163.com>
2023-09-17 11:18:41 +0800

    ✨ update controller-runtime to 0.16.2 (#3625)
    update controller-runtime to 0.16.2
    
    Signed-off-by: lowang-bh <lhui_wang@163.com>

--- testdata/project-v4-multigroup/internal/controller/sea-creatures/kraken_controller.go
+++ testdata/project-v4-multigroup/internal/controller/sea-creatures/kraken_controller.go
@@ -45,7 +45,7 @@ type KrakenReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.1/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.2/pkg/reconcile
 func (r *KrakenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
--- testdata/project-v4-with-grafana/go.mod
+++ testdata/project-v4-with-grafana/go.mod
@@ -5,7 +5,7 @@ go 1.20
 require (
 	k8s.io/apimachinery v0.28.1
 	k8s.io/client-go v0.28.1
-	sigs.k8s.io/controller-runtime v0.16.1
+	sigs.k8s.io/controller-runtime v0.16.2
 )
 
 require (
