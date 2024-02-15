1d28efd23241299e5e72432c8cde961b38619802
Marco Cadetg <marco.cadetg@amazee.io>
2024-02-15 21:48:23 +0100

    generate testdata samples

--- testdata/project-v4/test/e2e/e2e_test.go
+++ testdata/project-v4/test/e2e/e2e_test.go
@@ -74,6 +74,7 @@ var _ = Describe("controller", Ordered, func() {
 			By("installing CRDs")
 			cmd = exec.Command("make", "install")
 			_, err = utils.Run(cmd)
+			ExpectWithOffset(1, err).NotTo(HaveOccurred())
 
 			By("deploying the controller-manager")
 			cmd = exec.Command("make", "deploy", fmt.Sprintf("IMG=%s", projectimage))
