cc338d729c2a578ae491860e3eb71e63864b1390
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2024-03-30 07:58:43 +0000

    :sparkles: Upgrade controller-runtime from v0.17.0 to v0.17.2 (#3827)

--- pkg/plugins/golang/v4/scaffolds/init.go
+++ pkg/plugins/golang/v4/scaffolds/init.go
@@ -36,7 +36,7 @@ import (
 
 const (
 	// ControllerRuntimeVersion is the kubernetes-sigs/controller-runtime version to be used in the project
-	ControllerRuntimeVersion = "v0.17.0"
+	ControllerRuntimeVersion = "v0.17.2"
 	// ControllerToolsVersion is the kubernetes-sigs/controller-tools version to be used in the project
 	ControllerToolsVersion = "v0.14.0"
 	// EnvtestK8SVersion is the k8s version used to do the scaffold
