5ae816043233c59f6a260d3e184d67ec1158e410
Camila Macedo <camilamacedo86@gmail.com>
2024-04-02 19:02:05 +0100

    Change the Registry to registry.k8s.io

--- build-legacy/build.sh
+++ build-legacy/build.sh
@@ -22,7 +22,7 @@
 set -eu
 
 SOURCE_IMAGE_TAG="quay.io/brancz/kube-rbac-proxy:${KUBE_RBAC_PROXY_VERSION}"
-TARGET_IMAGE_TAG="gcr.io/kubebuilder/kube-rbac-proxy:${KUBE_RBAC_PROXY_VERSION}"
+TARGET_IMAGE_TAG="registry.k8s.io/kubebuilder/kube-rbac-proxy:${KUBE_RBAC_PROXY_VERSION}"
 
 # Each arch to pull an image for.
 declare ARCHES
