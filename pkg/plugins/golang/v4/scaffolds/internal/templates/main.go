1ab2a1464d9bb7702e49823b7149bb8ccf48ba5b
Camila Macedo <camilamacedo86@gmail.com>
2024-04-27 07:18:17 +0100

    Fix Invalid leader election ID generated when domain is empty

--- pkg/plugins/golang/v4/scaffolds/internal/templates/main.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/main.go
@@ -279,7 +279,11 @@ func main() {
 		WebhookServer: webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "{{ hashFNV .Repo }}.{{ .Domain }}",
+		{{- if not .Domain }}
+		LeaderElectionID:        "{{ hashFNV .Repo }}",
+		{{- else }}
+		LeaderElectionID:        "{{ hashFNV .Repo }}.{{ .Domain }}",
+		{{- end }}
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
