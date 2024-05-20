8387989e5cdc03095c8b96ceadc91d8b7c0111e8
Camila Macedo <camilamacedo86@gmail.com>
2024-05-20 06:36:38 +0100

    fix: makefile: target docker-buildx should use the project name insteaf of fix value

--- pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
@@ -185,10 +185,10 @@ PLATFORMS ?= linux/arm64,linux/amd64,linux/s390x,linux/ppc64le
 docker-buildx: ## Build and push docker image for the manager for cross-platform support
 	# copy existing Dockerfile and insert --platform=${BUILDPLATFORM} into Dockerfile.cross, and preserve the original Dockerfile
 	sed -e '1 s/\(^FROM\)/FROM --platform=\$$\{BUILDPLATFORM\}/; t' -e ' 1,// s//FROM --platform=\$$\{BUILDPLATFORM\}/' Dockerfile > Dockerfile.cross
-	- $(CONTAINER_TOOL) buildx create --name project-v3-builder
-	$(CONTAINER_TOOL) buildx use project-v3-builder
+	- $(CONTAINER_TOOL) buildx create --name {{ .ProjectName }}-builder
+	$(CONTAINER_TOOL) buildx use {{ .ProjectName }}-builder
 	- $(CONTAINER_TOOL) buildx build --push --platform=$(PLATFORMS) --tag ${IMG} -f Dockerfile.cross .
-	- $(CONTAINER_TOOL) buildx rm project-v3-builder
+	- $(CONTAINER_TOOL) buildx rm {{ .ProjectName }}-builder
 	rm Dockerfile.cross
 
 .PHONY: build-installer
