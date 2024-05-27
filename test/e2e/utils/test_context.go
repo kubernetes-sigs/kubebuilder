26de9e67db5c192188ccd92a6bdc1de8945dc004
Camila Macedo <camilamacedo86@gmail.com>
2024-05-27 19:54:31 +0100

    cleanup e2e tests for go/v4

--- test/e2e/utils/test_context.go
+++ test/e2e/utils/test_context.go
@@ -270,11 +270,18 @@ func (t *TestContext) CreateManagerNamespace() error {
 	return err
 }
 
-// LabelAllNamespacesToWarnAboutRestricted will label all namespaces so that we can verify
+// LabelNamespacesToWarnAboutRestricted will label all namespaces so that we can verify
 // if a warning with `Warning: would violate PodSecurity` will be raised when the manifests are applied
-func (t *TestContext) LabelAllNamespacesToWarnAboutRestricted() error {
-	_, err := t.Kubectl.Command("label", "--overwrite", "ns", "--all",
+func (t *TestContext) LabelNamespacesToWarnAboutRestricted() error {
+	_, err := t.Kubectl.Command("label", "--overwrite", "ns", t.Kubectl.Namespace,
 		"pod-security.kubernetes.io/warn=restricted")
+	return err
+}
+
+// RemoveNamespaceLabelToWarnAboutRestricted will remove the `pod-security.kubernetes.io/warn` label
+// from the specified namespace
+func (t *TestContext) RemoveNamespaceLabelToWarnAboutRestricted() error {
+	_, err := t.Kubectl.Command("label", "ns", t.Kubectl.Namespace, "pod-security.kubernetes.io/warn-")
 	return err
 }
 
