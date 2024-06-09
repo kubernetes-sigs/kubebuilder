adff563349c4290a60e00577847e1dc21bd18bd1
Camila Macedo <camilamacedo86@gmail.com>
2024-06-09 08:47:34 +0100

    Upgrade controller-runtime from v0.18.3 to v0.18.4

--- docs/book/src/cronjob-tutorial/testdata/project/go.mod
+++ docs/book/src/cronjob-tutorial/testdata/project/go.mod
@@ -11,7 +11,7 @@ require (
 	k8s.io/api v0.30.1
 	k8s.io/apimachinery v0.30.1
 	k8s.io/client-go v0.30.1
-	sigs.k8s.io/controller-runtime v0.18.3
+	sigs.k8s.io/controller-runtime v0.18.4
 )
 
 require (
--- pkg/plugins/golang/v4/scaffolds/init.go
+++ pkg/plugins/golang/v4/scaffolds/init.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	// ControllerRuntimeVersion is the kubernetes-sigs/controller-runtime version to be used in the project
-	ControllerRuntimeVersion = "v0.18.3"
+	ControllerRuntimeVersion = "v0.18.4"
 	// ControllerToolsVersion is the kubernetes-sigs/controller-tools version to be used in the project
 	ControllerToolsVersion = "v0.15.0"
 	// EnvtestK8SVersion is the k8s version used to do the scaffold
