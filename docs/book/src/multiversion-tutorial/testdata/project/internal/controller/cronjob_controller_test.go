1c6ef016d54de1576cb9b88e4c7a3f092b38e21e
Camila Macedo <camilamacedo86@gmail.com>
2024-08-12 07:05:05 +0100

    doc: remove fixme from controller-test for cronjob

--- docs/book/src/multiversion-tutorial/testdata/project/internal/controller/cronjob_controller_test.go
+++ docs/book/src/multiversion-tutorial/testdata/project/internal/controller/cronjob_controller_test.go
@@ -18,14 +18,14 @@ package controller
 
 import (
 	"context"
-	kbatch "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	kbatch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	batchv1 "tutorial.kubebuilder.io/project/api/v1"
 )
 
@@ -46,21 +46,24 @@ var _ = Describe("CronJob Controller", func() {
 			err := k8sClient.Get(ctx, typeNamespacedName, cronjob)
 			if err != nil && errors.IsNotFound(err) {
 				resource := &batchv1.CronJob{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "batch.tutorial.kubebuilder.io/v1",
+						Kind:       "CronJob",
+					},
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      resourceName,
-						Namespace: "default",
+						Namespace: typeNamespacedName.Namespace,
 					},
 					Spec: batchv1.CronJobSpec{
-						Schedule: "*/1 * * * *", // Example: runs every minute
+						Schedule: "1 * * * *",
 						JobTemplate: kbatch.JobTemplateSpec{
 							Spec: kbatch.JobSpec{
 								Template: corev1.PodTemplateSpec{
 									Spec: corev1.PodSpec{
 										Containers: []corev1.Container{
 											{
-												Name:    "example-container",
-												Image:   "example-image",
-												Command: []string{"echo", "Hello World"},
+												Name:  "test-container",
+												Image: "test-image",
 											},
 										},
 										RestartPolicy: corev1.RestartPolicyOnFailure,
@@ -75,7 +78,6 @@ var _ = Describe("CronJob Controller", func() {
 		})
 
 		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
 			resource := &batchv1.CronJob{}
 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
@@ -83,21 +85,19 @@ var _ = Describe("CronJob Controller", func() {
 			By("Cleanup the specific resource instance CronJob")
 			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 		})
-		// TODO: Fix me. We need to implement the tests and ensure
-		// that the controller implementation of multi-version tutorial is accurate
-		//It("should successfully reconcile the resource", func() {
-		//	By("Reconciling the created resource")
-		//	controllerReconciler := &CronJobReconciler{
-		//		Client: k8sClient,
-		//		Scheme: k8sClient.Scheme(),
-		//	}
-		//
-		//	_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-		//		NamespacedName: typeNamespacedName,
-		//	})
-		//	Expect(err).NotTo(HaveOccurred())
-		//	// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-		//	// Example: If you expect a certain status condition after reconciliation, verify it here.
-		//})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &CronJobReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
 	})
 })
