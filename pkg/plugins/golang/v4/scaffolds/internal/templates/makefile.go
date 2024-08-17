68fdd559ffcaba3e39d3918972753b6d27d8cbc8
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2024-08-17 07:33:26 +0100

    sparkles: Add support to k8s 1.31 and upgrade dependencies (#4080)
    Add support to k8s 1.31

--- pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
@@ -74,7 +74,7 @@ func (f *Makefile) SetTemplateDefaults() error {
 const makefileTemplate = `# Image URL to use all building/pushing image targets
 IMG ?= {{ .Image }}
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
-ENVTEST_K8S_VERSION = 1.30.0
+ENVTEST_K8S_VERSION = 1.31.0
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
