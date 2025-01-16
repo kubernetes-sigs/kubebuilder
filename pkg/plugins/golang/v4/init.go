0e988a6556ddf2f23728447fd1979621318aeb86
Dmitry Volodin <dmvolod@gmail.com>
2025-01-16 15:39:42 +0300

    :bug: Update golang version to 1.23.0 in init validation logic

--- pkg/plugins/golang/v4/init.go
+++ pkg/plugins/golang/v4/init.go
@@ -36,7 +36,7 @@ import (
 
 // Variables and function to check Go version requirements.
 var (
-	goVerMin = golang.MustParse("go1.19.0")
+	goVerMin = golang.MustParse("go1.23.0")
 	goVerMax = golang.MustParse("go2.0alpha1")
 )
 
