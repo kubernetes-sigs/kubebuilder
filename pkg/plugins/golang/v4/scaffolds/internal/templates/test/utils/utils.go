40ece861b6ec5ab28694f7041baad1cc63d13b3d
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-01-17 00:15:07 +0000

    Upgrade cert-manager version used from v1.16.0 to v1.16.3

--- pkg/plugins/golang/v4/scaffolds/internal/templates/test/utils/utils.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/test/utils/utils.go
@@ -59,7 +59,7 @@ const (
 	prometheusOperatorURL     = "https://github.com/prometheus-operator/prometheus-operator/" +
 		"releases/download/%s/bundle.yaml"
 
-	certmanagerVersion = "v1.16.0"
+	certmanagerVersion = "v1.16.3"
 	certmanagerURLTmpl = "https://github.com/jetstack/cert-manager/releases/download/%s/cert-manager.yaml"
 )
 
