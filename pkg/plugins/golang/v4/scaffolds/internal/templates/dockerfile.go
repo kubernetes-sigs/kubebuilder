51115f7ed7e2897b7d86361194415c2699f4abd1
pg2000 <10741029+PG2000@users.noreply.github.com>
2025-01-26 20:53:02 +0100

    chore: remove race flag in for go build in Dockerfile

--- pkg/plugins/golang/v4/scaffolds/internal/templates/dockerfile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/dockerfile.go
@@ -61,7 +61,7 @@ COPY internal/ internal/
 # was called. For example, if we call make docker-build in a local env which has the Apple Silicon M1 SO
 # the docker BUILDPLATFORM arg will be linux/arm64 when for Apple x86 it will be linux/amd64. Therefore,
 # by leaving it empty we can ensure that the container and binary shipped on it will have the same platform.
-RUN CGO_ENABLED=0 GOOS=${TARGETOS:-linux} GOARCH=${TARGETARCH} go build -race -a -o manager cmd/main.go
+RUN CGO_ENABLED=0 GOOS=${TARGETOS:-linux} GOARCH=${TARGETARCH} go build -a -o manager cmd/main.go
 
 # Use distroless as minimal base image to package the manager binary
 # Refer to https://github.com/GoogleContainerTools/distroless for more details
