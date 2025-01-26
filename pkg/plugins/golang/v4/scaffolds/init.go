2d657c56a7114e6150827c77b2995a08ec90646a
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-01-26 12:05:39 +0000

    ✨ Upgrade controller-runtime from v0.20.0 to v0.20.1 (#4528)
    Upgrade controller-runtime from v0.20.0 to v0.20.1

--- pkg/plugins/golang/v4/scaffolds/init.go
+++ pkg/plugins/golang/v4/scaffolds/init.go
@@ -39,7 +39,7 @@ const (
 	// GolangciLintVersion is the golangci-lint version to be used in the project
 	GolangciLintVersion = "v1.63.4"
 	// ControllerRuntimeVersion is the kubernetes-sigs/controller-runtime version to be used in the project
-	ControllerRuntimeVersion = "v0.20.0"
+	ControllerRuntimeVersion = "v0.20.1"
 	// ControllerToolsVersion is the kubernetes-sigs/controller-tools version to be used in the project
 	ControllerToolsVersion = "v0.17.1"
 
