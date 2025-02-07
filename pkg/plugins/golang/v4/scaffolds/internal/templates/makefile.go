8d71d0cc966f8f4770e28093fa279c3d28e09213
Tiger Kaovilai <tkaovila@redhat.com>
2025-02-07 10:14:16 -0500

    v4 template docker-buildx do not add duplicate --platform

--- pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
@@ -199,7 +199,7 @@ PLATFORMS ?= linux/arm64,linux/amd64,linux/s390x,linux/ppc64le
 .PHONY: docker-buildx
 docker-buildx: ## Build and push docker image for the manager for cross-platform support
 	# copy existing Dockerfile and insert --platform=${BUILDPLATFORM} into Dockerfile.cross, and preserve the original Dockerfile
-	sed -e '1 s/\(^FROM\)/FROM --platform=\$$\{BUILDPLATFORM\}/; t' -e ' 1,// s//FROM --platform=\$$\{BUILDPLATFORM\}/' Dockerfile > Dockerfile.cross
+	(cat Dockerfile | grep -qe '--platform' && cp Dockerfile Dockerfile.cross) || sed -e '1 s/\(^FROM\)/FROM --platform=\$$\{BUILDPLATFORM\}/; t' -e ' 1,// s//FROM --platform=\$$\{BUILDPLATFORM\}/' Dockerfile > Dockerfile.cross
 	- $(CONTAINER_TOOL) buildx create --name {{ .ProjectName }}-builder
 	$(CONTAINER_TOOL) buildx use {{ .ProjectName }}-builder
 	- $(CONTAINER_TOOL) buildx build --push --platform=$(PLATFORMS) --tag ${IMG} -f Dockerfile.cross .
