425a5eaaa5c531bff434d913d3d244e25aa6ee9b
Tiger Kaovilai <tkaovila@redhat.com>
2025-02-07 10:25:58 -0500

    Makefile: docker-build should use --load for newer docker build aliased to buildx

--- pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/makefile.go
@@ -183,7 +183,7 @@ run: manifests generate fmt vet ## Run a controller from your host.
 # More info: https://docs.docker.com/develop/develop-images/build_enhancements/
 .PHONY: docker-build
 docker-build: ## Build docker image with the manager.
-	$(CONTAINER_TOOL) build -t ${IMG} .
+	$(CONTAINER_TOOL) build --load -t ${IMG} .
 
 .PHONY: docker-push
 docker-push: ## Push docker image with the manager.
