7c4b6c9b4b093dee2b610dd9bbbbe91527487302
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-02-11 06:25:28 +0000

    (go/v4,ksutomize/v2,helm/v1-alpha): Fix prometheus integration with TLS check
    Co-Author: Abhisek Dwivedi <abhishekdwivedi60@gmail.com>

--- pkg/plugins/optional/helm/v1alpha/scaffolds/internal/templates/chart-templates/prometheus/monitor.go
+++ pkg/plugins/optional/helm/v1alpha/scaffolds/internal/templates/chart-templates/prometheus/monitor.go
@@ -59,6 +59,7 @@ spec:
       bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
       tlsConfig:
         {{ "{{- if .Values.certmanager.enable }}" }}
+        serverName: {{ .ProjectName }}-controller-manager-metrics-service.{{ "{{ .Release.Namespace }}" }}.svc
         # Apply secure TLS configuration with cert-manager
         insecureSkipVerify: false
         ca:
