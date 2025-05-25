83c6aa0b33cf4dda115591af423be19d25e4e57b
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-05-25 11:56:25 +0100

    feat: improve UX and maintainability by auto-uncommenting webhook blocks
    This change enhances the user experience by automatically uncommenting
    webhook-related code blocks during scaffolding. It also improves
    maintainability by eliminating the need to manually uncomment code when
    running end-to-end tests for examples and mocks.

--- pkg/plugins/common/kustomize/v2/scaffolds/webhook.go
+++ pkg/plugins/common/kustomize/v2/scaffolds/webhook.go
@@ -35,7 +35,10 @@ import (
 
 var _ plugins.Scaffolder = &webhookScaffolder{}
 
-const kustomizeFilePath = "config/default/kustomization.yaml"
+const (
+	kustomizeFilePath    = "config/default/kustomization.yaml"
+	kustomizeCRDFilePath = "config/crd/kustomization.yaml"
+)
 
 type webhookScaffolder struct {
 	config   config.Config
@@ -107,15 +110,200 @@ func (s *webhookScaffolder) Scaffold() error {
 		return fmt.Errorf("error scaffolding kustomize webhook manifests: %w", err)
 	}
 
-	policyKustomizeFilePath := "config/network-policy/kustomization.yaml"
-	err := pluginutil.InsertCodeIfNotExist(policyKustomizeFilePath,
-		"resources:", allowWebhookTrafficFragment)
+	// Apply project-specific customizations:
+	// - Add reference to allow-webhook-traffic.yaml in network policy configuration.
+	// - Enable all webhook-related sections in config/default/kustomization.yaml.
+	addNetworkPoliciesForWebhooks()
+	// enableWebhookDefaults ensures all necessary components for webhook functionality
+	// are enabled in config/default/kustomization.yaml, including:
+	// - webhook and cert-manager directories
+	// - manager patches
+	// - replacements for certificate injection
+	enableWebhookDefaults()
+	if s.resource.HasValidationWebhook() {
+		uncommentCodeForValidationWebhooks()
+	}
+	if s.resource.HasDefaultingWebhook() {
+		uncommentCodeForDefaultWebhooks()
+	}
+	if s.resource.HasConversionWebhook() {
+		uncommentCodeForConversionWebhooks(s.resource)
+	}
+	return nil
+}
+
+// uncommentCodeForConversionWebhooks enables CA injection logic in Kustomize manifests
+// for ConversionWebhooks by uncommenting certificate sources and CRD annotation targets.
+// This is required to make cert-manager correctly inject the CA bundle into CRDs.
+func uncommentCodeForConversionWebhooks(r resource.Resource) {
+	crdName := fmt.Sprintf("%s.%s", r.Plural, r.QualifiedGroup())
+	err := pluginutil.UncommentCode(
+		kustomizeFilePath,
+		fmt.Sprintf(`# - source: # Uncomment the following block if you have a ConversionWebhook (--conversion)
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert
+#     fieldPath: .metadata.namespace # Namespace of the certificate CR
+#   targets: # Do not remove or uncomment the following scaffold marker; required to generate code for target CRD.
+#     - select:
+#         kind: CustomResourceDefinition
+#         name: %s
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 0
+#         create: true`, crdName),
+		"#",
+	)
 	if err != nil {
-		log.Errorf("Unable to add the line '- allow-webhook-traffic.yaml' at the end of the file"+
-			"%s to allow webhook traffic.", policyKustomizeFilePath)
+		log.Warningf("Unable to find the certificate namespace replacement for "+
+			"CRD %s to uncomment in %s. Conversion webhooks require this replacement "+
+			"to inject the CA properly.",
+			crdName, kustomizeFilePath)
+	}
+	err = pluginutil.UncommentCode(
+		kustomizeFilePath,
+		fmt.Sprintf(`# - source:
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert
+#     fieldPath: .metadata.name
+#   targets: # Do not remove or uncomment the following scaffold marker; required to generate code for target CRD.
+#     - select:
+#         kind: CustomResourceDefinition
+#         name: %s
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 1
+#         create: true`, crdName),
+		"#",
+	)
+	if err != nil {
+		log.Warningf("Unable to find the certificate name replacement for CRD %s "+
+			"to uncomment in %s. Conversion webhooks require this replacement to inject "+
+			"the CA properly.",
+			crdName, kustomizeFilePath)
 	}
 
-	err = pluginutil.UncommentCode(kustomizeFilePath, "#- ../webhook", `#`)
+	err = pluginutil.UncommentCode(kustomizeCRDFilePath, `#configurations:
+#- kustomizeconfig.yaml`, `#`)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeCRDFilePath,
+			`configurations:
+- kustomizeconfig.yaml`)
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the target configurations with kustomizeconfig.yaml"+
+				"to uncomment in the file %s. ConverstionWebhooks requires this configuration "+
+				"to be uncommented to inject CA", kustomizeCRDFilePath)
+		}
+	}
+}
+
+func uncommentCodeForDefaultWebhooks() {
+	err := pluginutil.UncommentCode(
+		kustomizeFilePath,
+		`# - source: # Uncomment the following block if you have a DefaultingWebhook (--defaulting )
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert
+#     fieldPath: .metadata.namespace # Namespace of the certificate CR
+#   targets:
+#     - select:
+#         kind: MutatingWebhookConfiguration
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 0
+#         create: true
+# - source:
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert
+#     fieldPath: .metadata.name
+#   targets:
+#     - select:
+#         kind: MutatingWebhookConfiguration
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 1
+#         create: true`,
+		"#",
+	)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath,
+			`   targets:
+     - select:
+         kind: MutatingWebhookConfiguration`)
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the MutatingWebhookConfiguration section "+
+				"to uncomment in %s. Webhooks scaffolded with '--defaulting' require "+
+				"this configuration for CA injection.",
+				kustomizeFilePath)
+		}
+	}
+}
+
+func uncommentCodeForValidationWebhooks() {
+	err := pluginutil.UncommentCode(
+		kustomizeFilePath,
+		`# - source: # Uncomment the following block if you have a ValidatingWebhook (--programmatic-validation)
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert # This name should match the one in certificate.yaml
+#     fieldPath: .metadata.namespace # Namespace of the certificate CR
+#   targets:
+#     - select:
+#         kind: ValidatingWebhookConfiguration
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 0
+#         create: true
+# - source:
+#     kind: Certificate
+#     group: cert-manager.io
+#     version: v1
+#     name: serving-cert
+#     fieldPath: .metadata.name
+#   targets:
+#     - select:
+#         kind: ValidatingWebhookConfiguration
+#       fieldPaths:
+#         - .metadata.annotations.[cert-manager.io/inject-ca-from]
+#       options:
+#         delimiter: '/'
+#         index: 1
+#         create: true`,
+		"#",
+	)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath,
+			`   targets:
+     - select:
+         kind: ValidatingWebhookConfiguration`)
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the ValidatingWebhookConfiguration section "+
+				"to uncomment in %s. Webhooks scaffolded with '--programmatic-validation' "+
+				"require this configuration for CA injection.",
+				kustomizeFilePath)
+		}
+	}
+}
+
+func enableWebhookDefaults() {
+	err := pluginutil.UncommentCode(kustomizeFilePath, "#- ../webhook", `#`)
 	if err != nil {
 		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath, "- ../webhook")
 		if !hasWebHookUncommented || errCheck != nil {
@@ -145,21 +333,93 @@ func (s *webhookScaffolder) Scaffold() error {
 		}
 	}
 
-	if s.resource.Webhooks.Conversion {
-		crdKustomizationsFilePath := "config/crd/kustomization.yaml"
-		err = pluginutil.UncommentCode(crdKustomizationsFilePath, "#configurations:\n#- kustomizeconfig.yaml", `#`)
-		if err != nil {
-			hasWebHookUncommented, err := pluginutil.HasFileContentWith(crdKustomizationsFilePath,
-				"configurations:\n- kustomizeconfig.yaml")
-			if !hasWebHookUncommented || err != nil {
-				log.Warningf("Unable to find the target(s) configurations.kustomizeconfig.yaml "+
-					"to uncomment in the file "+
-					"%s.", crdKustomizationsFilePath)
-			}
+	err = pluginutil.UncommentCode(kustomizeFilePath, `#- ../certmanager`, `#`)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath,
+			"../certmanager")
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the '../certmanager' section to uncomment in %s. "+
+				"Projects that use webhooks must enable certificate management."+
+				"Please ensure cert-manager integration is enabled.",
+				kustomizeFilePath)
 		}
 	}
 
-	return nil
+	err = pluginutil.UncommentCode(kustomizeFilePath, `#replacements:`, `#`)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath,
+			"replacements:")
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the '#replacements:' section to uncomment in %s."+
+				"Projects using webhooks must enable cert-manager CA injection by uncommenting"+
+				"the required replacements.",
+				kustomizeFilePath)
+		}
+	}
+
+	err = pluginutil.UncommentCode(
+		kustomizeFilePath,
+		`# - source: # Uncomment the following block if you have any webhook
+#     kind: Service
+#     version: v1
+#     name: webhook-service
+#     fieldPath: .metadata.name # Name of the service
+#   targets:
+#     - select:
+#         kind: Certificate
+#         group: cert-manager.io
+#         version: v1
+#         name: serving-cert
+#       fieldPaths:
+#         - .spec.dnsNames.0
+#         - .spec.dnsNames.1
+#       options:
+#         delimiter: '.'
+#         index: 0
+#         create: true
+# - source:
+#     kind: Service
+#     version: v1
+#     name: webhook-service
+#     fieldPath: .metadata.namespace # Namespace of the service
+#   targets:
+#     - select:
+#         kind: Certificate
+#         group: cert-manager.io
+#         version: v1
+#         name: serving-cert
+#       fieldPaths:
+#         - .spec.dnsNames.0
+#         - .spec.dnsNames.1
+#       options:
+#         delimiter: '.'
+#         index: 1
+#         create: true`,
+		"#",
+	)
+	if err != nil {
+		hasWebHookUncommented, errCheck := pluginutil.HasFileContentWith(kustomizeFilePath,
+			`     kind: Service
+     version: v1
+     name: webhook-service
+     fieldPath: .metadata.name`)
+		if !hasWebHookUncommented || errCheck != nil {
+			log.Warningf("Unable to find the '#- source: # Uncomment the following block if you have any webhook' "+
+				"section to uncomment in %s. "+
+				"Projects with webhooks must enable certificates via cert-manager.",
+				kustomizeFilePath)
+		}
+	}
+}
+
+func addNetworkPoliciesForWebhooks() {
+	policyKustomizeFilePath := "config/network-policy/kustomization.yaml"
+	err := pluginutil.InsertCodeIfNotExist(policyKustomizeFilePath,
+		"resources:", allowWebhookTrafficFragment)
+	if err != nil {
+		log.Errorf("Unable to add the line '- allow-webhook-traffic.yaml' at the end of the file"+
+			"%s to allow webhook traffic.", policyKustomizeFilePath)
+	}
 }
 
 // Deprecated: remove it when go/v4 and/or kustomize/v2 be removed
