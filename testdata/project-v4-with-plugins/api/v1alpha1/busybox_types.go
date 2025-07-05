04cdec97b870001e362de1212c16e6848a805474
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-07-05 21:45:57 +0100

    (deploy-image/v1-alpha1): Remove default max size validation

--- testdata/project-v4-with-plugins/api/v1alpha1/busybox_types.go
+++ testdata/project-v4-with-plugins/api/v1alpha1/busybox_types.go
@@ -32,8 +32,6 @@ type BusyboxSpec struct {
 	// The following markers will use OpenAPI v3 schema to validate the value
 	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 	// +kubebuilder:validation:Minimum=1
-	// +kubebuilder:validation:Maximum=3
-	// +kubebuilder:validation:ExclusiveMaximum=false
 	Size int32 `json:"size,omitempty"`
 }
 
