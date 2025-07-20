7ec818e426d30c7c9efc12e406b20c434c193f12
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-07-20 09:59:03 +0100

    (CLI;go/v1) Upgrade golang version from 1.24.0 to 1.24.5

--- pkg/plugins/golang/v4/scaffolds/internal/templates/gomod.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/gomod.go
@@ -45,7 +45,7 @@ func (f *GoMod) SetTemplateDefaults() error {
 
 const goModTemplate = `module {{ .Repo }}
 
-go 1.24.0
+go 1.24.5
 
 require (
 	sigs.k8s.io/controller-runtime {{ .ControllerRuntimeVersion }}
