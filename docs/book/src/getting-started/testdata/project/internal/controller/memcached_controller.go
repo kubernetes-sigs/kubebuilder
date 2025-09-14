334fec078a2133173a22834d7fd7c596a26b2d53
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-09-14 16:20:45 +0100

    Upgrade controller-runtime from v0.22.0 to v0.22.1

--- docs/book/src/getting-started/testdata/project/internal/controller/memcached_controller.go
+++ docs/book/src/getting-started/testdata/project/internal/controller/memcached_controller.go
@@ -68,7 +68,7 @@ type MemcachedReconciler struct {
 // - About Controllers: https://kubernetes.io/docs/concepts/architecture/controller/
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.1/pkg/reconcile
 func (r *MemcachedReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logf.FromContext(ctx)
 
--- docs/book/src/multiversion-tutorial/testdata/project/internal/controller/cronjob_controller.go
+++ docs/book/src/multiversion-tutorial/testdata/project/internal/controller/cronjob_controller.go
@@ -95,7 +95,7 @@ var (
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.1/pkg/reconcile
 // nolint:gocyclo
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logf.FromContext(ctx)
