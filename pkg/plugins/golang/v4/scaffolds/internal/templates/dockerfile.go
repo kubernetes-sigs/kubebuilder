1e5badae7fd0b80ec34422cf5889c199696a5889
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-10-13 09:06:48 +0100

    Upgrade go

--- pkg/plugins/golang/v4/scaffolds/internal/templates/dockerfile.go
+++ pkg/plugins/golang/v4/scaffolds/internal/templates/dockerfile.go
@@ -39,7 +39,7 @@ func (f *Dockerfile) SetTemplateDefaults() error {
 }
 
 const dockerfileTemplate = `# Build the manager binary
-FROM golang:1.24 AS builder
+FROM golang:1.25 AS builder
 ARG TARGETOS
 ARG TARGETARCH
 
