704fbc5c7d87b0716c6753682087ac9332a605ad
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-11-04 09:36:01 +0000

    ✨ Upgrade controller-runtime from 0.22.3 to 0.22.4 (#5178)
    Upgrade controller-runtime from 0.22.3 to 0.22.4

--- docs/book/src/cronjob-tutorial/testdata/project/go.mod
+++ docs/book/src/cronjob-tutorial/testdata/project/go.mod
@@ -10,7 +10,7 @@ require (
 	k8s.io/apimachinery v0.34.1
 	k8s.io/client-go v0.34.1
 	k8s.io/utils v0.0.0-20250604170112-4c0f3b243397
-	sigs.k8s.io/controller-runtime v0.22.3
+	sigs.k8s.io/controller-runtime v0.22.4
 )
 
 require (
--- testdata/project-v4-multigroup/internal/controller/sea-creatures/kraken_controller.go
+++ testdata/project-v4-multigroup/internal/controller/sea-creatures/kraken_controller.go
@@ -45,7 +45,7 @@ type KrakenReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.3/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
 func (r *KrakenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = logf.FromContext(ctx)
 
