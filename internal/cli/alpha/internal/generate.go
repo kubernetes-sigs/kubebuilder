a54d3837d9627d509bcf88e25ec916286de6f66d
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2026-03-29 15:02:06 -0300

    docs(helm/v1alpha): Add deprecation notice
    Add deprecation notices to the helm/v1alpha and helm/v1alpha/scaffolds
    packages to warn users about importing these deprecated plugin packages.
    
    Users are directed to use helm/v2-alpha plugin packages instead.

--- internal/cli/alpha/internal/generate.go
+++ internal/cli/alpha/internal/generate.go
@@ -33,7 +33,7 @@ import (
 	deployimagev1alpha1 "sigs.k8s.io/kubebuilder/v4/pkg/plugins/golang/deploy-image/v1alpha1"
 	autoupdatev1alpha "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/autoupdate/v1alpha"
 	grafanav1alpha "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/grafana/v1alpha"
-	helmv1alpha "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/helm/v1alpha"
+	helmv1alpha "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/helm/v1alpha" //nolint:staticcheck // Deprecated
 	helmv2alpha "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/helm/v2alpha"
 )
 
